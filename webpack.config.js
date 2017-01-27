b85074e686b64bad994efdbc779c4b5f8bccb215
Timothy Chung <timchunght@gmail.com>
2017-01-27 18:34:51 -0500

    Updated CssEntry and added yarn

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
-  entry: "./app/App.js",
+  entry: {
+    js: "./app/App.js",
+    css: "./app/CssEntry.js"
+  },
   output: {
-    filename: "public/bundle.js"
+    filename: "public/dist/bundle.js"
   },
   module: {
     loaders: [
@@ -12,7 +17,27 @@ module.exports = {
         query: {
           presets: ['react', 'es2015']
         }
+      },
+      // Extract css files
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+      },
+      // Optionally extract less files
+      // or any other compile-to-css language
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
+      },
+      {
+        test: /\.(png|eot|svg|ttf|woff(2)?)(\?v=\d+\.\d+\.\d+)?/,
+        loader: 'url'
       }
     ]
-  }
+  },
+  // Use the plugin to specify the resulting filename (and add needed behavior to the compiler)
+  plugins: [
+    new ExtractTextPlugin("public/dist/bundle.css")
+  ]
+
 }
